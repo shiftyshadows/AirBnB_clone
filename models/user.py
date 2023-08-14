d5dfc91a77ce4d93308c23820c5f7635f1d18c72
Shiftyshadows <purple.quasar@proton.me>
2023-08-14 11:44:01 +0300

    corrections

--- models/user.py
+++ models/user.py
@@ -5,10 +5,10 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """ User class that inherits from BaseModel """
-    email = None
-    password = None
-    first_name = None
-    last_name = None
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
